4c418fa2fad0261d9daa127a0896e40cc9c88101
MarkNazar <markkristophern@gmail.com>
2024-07-02 11:39:07 +0800

    add sort latest first to expenses

--- src/services/apiExpenses.js
+++ src/services/apiExpenses.js
@@ -8,13 +8,18 @@ import {
   doc,
   serverTimestamp,
   updateDoc,
+  orderBy,
 } from "firebase/firestore";
 import { db } from "./firebase";
 
 const expensesRef = collection(db, "expenses");
 
 export const getExpenses = async (id) => {
-  const q = query(expensesRef, where("recordId", "==", id));
+  const q = query(
+    expensesRef,
+    where("recordId", "==", id),
+    orderBy("createdAt", "desc")
+  );
   const expensesSnapshot = await getDocs(q);
   const expenses = expensesSnapshot.docs.map((doc) => {
     return { data: doc.data(), id: doc.id };
